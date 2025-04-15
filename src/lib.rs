e5065e71d16771cf573007597922d9380912e8d9
Aran Varun <vir@tue.su>
2025-04-15 09:34:34 +0300

    compiled

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use base64::{engine::general_purpose::URL_SAFE_NO_PAD, Engine as _};
+use base64::{Engine as _, engine::general_purpose::URL_SAFE_NO_PAD};
 use rand_core::RngCore;
 use reqwest;
 use rsa::pkcs1::DecodeRsaPublicKey;
@@ -7,7 +7,7 @@ use rsa::{Pkcs1v15Encrypt, RsaPublicKey};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub async fn generate_aes_key() -> Result<(), JsValue> {
+pub async fn generate_aes_key() -> String {
     // Случайные данные для шифрования
     let mut rng = rand_core::OsRng;
     let mut data = [0u8; 16];
@@ -17,33 +17,31 @@ pub async fn generate_aes_key() -> Result<(), JsValue> {
     let public_key = RsaPublicKey::from_pkcs1_der(include_bytes!("public_key.der"))
         .expect("Failed to load public key");
 
-    let b64uridata = URL_SAFE_NO_PAD.encode(data);
     // Шифрование данных
     let encrypted_data = public_key
         .encrypt(&mut rng, Pkcs1v15Encrypt, &data)
         .expect("Encryption failed");
+    store_key(encrypted_data).await;
 
-    Ok(())
+    URL_SAFE_NO_PAD.encode(data)
 }
 
 #[wasm_bindgen]
-pub async fn store_key(encdata: Vec<u8>) -> Result<(), JsValue> {
+pub async fn store_key(encdata: Vec<u8>) {
     // Отправка данных на сервер
-    let url = "https://example.com/api/encrypted_data"; // Замените на нужный URL
+    let url = "https://example.com/api/encrypted_data";
     let client = reqwest::Client::new();
-    let response = client
-        .post(url)
-        .body(encdata)
-        .send()
-        .await
-        .map_err(|e| JsValue::from_str(&format!("Request error: {}", e)))?;
+    let response = client.post(url).body(encdata).send().await.unwrap();
 
     if response.status().is_success() {
-        Ok(())
+        log("Data sent successfully");
     } else {
-        Err(JsValue::from_str(&format!(
-            "Server error: {}",
-            response.status()
-        )))
+        log(response.status().as_str());
     }
+}
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log(s: &str);
 }
